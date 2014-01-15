970b250d68d39ecf44eac28f91d7e346dfb47542
Stephen Braitsch <stephen@quietless.com>
2014-01-15 11:53:31 -0800

    removing signup form and enabling download link

--- app/server/router.js
+++ app/server/router.js
@@ -5,25 +5,12 @@ module.exports = function(app) {
 		res.render('index');
 	});
 	
-	app.get('/beta', function(req, res){
-		res.render('beta');
-	});
-	
-	app.get('/download', function(req, res){
-		res.send(200);
-	});
-	
 	app.get('/help', function(req, res){
 		res.render('help');
 	});
 	
 	app.get('/issues', function(req, res){
 		res.render('issues');
-	});
-	
-	app.post('/signup', function(req, res){
-		console.log(req.param('name'), req.param('email'), req.param('platform'));
-		res.send('ok', 200);
 	});
 	
 	app.get('*', function(req, res){
