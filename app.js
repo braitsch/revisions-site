518c05632049d9e8838d9f8173c3ecd08c957b9c
braitsch <stephen@braitsch.io>
2015-10-19 10:48:47 -0700

    dependency updates

--- app.js
+++ app.js
@@ -4,25 +4,19 @@
  * Author :: Stephen Braitsch
  */
 
-var http = require('http');
 var express = require('express');
 var app = express();
-var server = http.createServer(app);
+var http = require('http').Server(app);
 
-app.configure(function(){
-	app.set('port', 8080);
-	app.set('views', __dirname + '/app/server/views');
-	app.set('view engine', 'jade');
-	app.locals.pretty = true;
-	app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(express.methodOverride());
-	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
-	app.use(express.static(__dirname + '/app/public'));
-});
+app.locals.pretty = true;
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
+app.set('views', './app/server/views');
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
+require('./app/server/routes')(app);
 
-require('./app/server/router')(app);
-
-server.listen(app.get('port'), function(){
-	console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env);
+http.listen(app.get('port'), function()
+{
+	console.log('Express server listening on port', app.get('port'));
 });
